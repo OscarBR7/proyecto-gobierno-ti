cbaaaf58508c93ad8740fb5f624a886e229062ec
OscarBR7 <oscarbrionesrayo10@gmail.com>
2026-01-04 22:24:08 -0600

    Configuración de despliegue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Para que las rutas funcionen en producción
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // Archivos más pequeños
+  }
 })
